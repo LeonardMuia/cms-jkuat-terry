375e92a362df5f5d22c5528ef870d36a8da07c8e
LeonardMuia <leonardmuia@outlook.com>
2026-06-23 20:43:59 +0300

    Updated models

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,11 @@
+# accounts/models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
+from django.conf import settings
 import uuid
-
 
 # ====================== YOUR EXISTING PROFILE (Improved) ======================
 class Profile(models.Model):
